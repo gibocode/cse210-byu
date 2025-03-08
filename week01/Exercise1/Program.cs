68d837b4cbcf957eb9fc1655ea8ba4a62289e4f1
Gilbor Camporazo Jr. <gilborcamporazojr@programmer.net>
2025-03-08 21:12:34 +0800

    Update variable name.

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,11 +5,11 @@ class Program
     static void Main(string[] args)
     {
         Console.Write("What is your first name? ");
-        string fname = Console.ReadLine();
+        string firstName = Console.ReadLine();
 
         Console.Write("What is your last name? ");
-        string lname = Console.ReadLine();
+        string lastName = Console.ReadLine();
 
-        Console.WriteLine($"\nYour name is {lname}, {fname} {lname}.");
+        Console.WriteLine($"\nYour name is {lastName}, {firstName} {lastName}.");
     }
 }
