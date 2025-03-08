d404cceda47a37a4d88e02742d5f660059176dc7
Gilbor Camporazo Jr. <gilborcamporazojr@programmer.net>
2025-03-08 22:13:51 +0800

    C# Prep 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new();
+        int magicNumber;
+
+        string guessNumberInput;
+        int guessNumber;
+        int guesses;
+        bool keepPlaying = true;
+
+        while (keepPlaying)
+        {
+            magicNumber = randomGenerator.Next(1, 100);
+            guesses = 0;
+
+            do
+            {
+                Console.Write("What is your guess? ");
+                guessNumberInput = Console.ReadLine();
+
+                guesses++;
+
+                guessNumber = int.Parse(guessNumberInput);
+
+                if (magicNumber > guessNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (magicNumber < guessNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+            } while (magicNumber != guessNumber);
+
+            Console.WriteLine($"You did {guesses} guesses.");
+            Console.Write("Do you want to play again? ");
+
+            keepPlaying = Console.ReadLine() == "yes";
+        }
+
+        Console.WriteLine("Thank you for playing!");
     }
 }
