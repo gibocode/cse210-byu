229a861fd50cc9f80bcd39a122db031f41afa96a
Gilbor Camporazo Jr. <gilborcamporazojr@programmer.net>
2025-03-08 21:46:32 +0800

    C# Prep 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,58 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("Enter your grade percentage: ");
+        string percentageInput = Console.ReadLine();
+
+        int percentage = int.Parse(percentageInput);
+        string letter;
+
+        if (percentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (percentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (percentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (percentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        int remainder = percentage % 10;
+        string sign;
+
+        if (remainder >= 7 && letter != "A" && letter != "F")
+        {
+            sign = "+";
+        }
+        else if (remainder < 3 && letter != "F")
+        {
+            sign = "-";
+        }
+        else
+        {
+            sign = "";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}{sign}.");
+
+        if (percentage >= 70)
+        {
+            Console.WriteLine("You passed! Congratulations!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, you didn't passed. Do your best next time! I know you can do it!");
+        }
     }
 }
