5e4b78cf2b55667b9bc2c807896af616c9b05dc2
Gilbor Camporazo Jr. <gilborcamporazojr@programmer.net>
2025-03-20 00:31:12 +0800

    Week 3 team activity scripture memorizer design.

---
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -0,0 +1,27 @@
+using System;
+
+public class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words = new List<Word>();
+
+    public Scripture(Reference reference, string text)
+    {
+        
+    }
+
+    private void HideRandomWords(int numberToHide)
+    {
+
+    }
+
+    private string GetDisplayText()
+    {
+        return "";
+    }
+
+    private bool IsCompletelyHidden()
+    {
+        return false;
+    }
+}
---
+++ week03/ScriptureMemorizer/Word.cs
@@ -0,0 +1,32 @@
+using System;
+
+public class Word
+{
+    private string _text;
+    private bool _isHidden;
+
+    public Word(string text)
+    {
+        
+    }
+
+    private void Hide()
+    {
+
+    }
+
+    private void Show()
+    {
+
+    }
+
+    private bool IsHidden()
+    {
+        return false;
+    }
+
+    private string GetDisplayText()
+    {
+        return "";
+    }
+}
