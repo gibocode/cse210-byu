f4a22b627d266ecbd3bf1772dd5c93175e754a12
Gilbor Camporazo Jr. <gilborcamporazojr@programmer.net>
2025-04-09 21:36:13 +0800

    Week 6 - Polymorphism

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(new Square("Blue", 4));
+        shapes.Add(new Rectangle("Red", 5, 10));
+        shapes.Add(new Circle("Green", 3));
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.GetColor()}");
+            Console.WriteLine($"Area: {shape.GetArea()}");
+        }
     }
 }
