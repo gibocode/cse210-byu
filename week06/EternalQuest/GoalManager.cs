d2816c5e456c0cb54004b0da7e01ccc9470949fe
Gilbor Camporazo Jr. <gilborcamporazojr@programmer.net>
2025-04-10 23:19:45 +0800

    Week 6 - Eternal Quest Design

---
+++ week06/EternalQuest/GoalManager.cs
@@ -0,0 +1,52 @@
+using System;
+
+public class GoalManager
+{
+    List<Goal> _goals = new List<Goal>();
+    private int _score;
+
+    public GoalManager()
+    {
+        
+    }
+
+    private void Start()
+    {
+
+    }
+
+    private void DisplayPlayerInfo()
+    {
+
+    }
+
+    private void ListGoalNames()
+    {
+
+    }
+
+    private void ListGoalDetails()
+    {
+
+    }
+
+    private void CreateGoal()
+    {
+
+    }
+
+    private void RecordEvent()
+    {
+
+    }
+
+    private void SaveGoals()
+    {
+
+    }
+
+    private void LoadGoals()
+    {
+
+    }
+}
